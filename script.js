1c94e49384e6290f60b150175a0dba65f308f630
Dylan PETERANO <dylan.peterano.work@gmail.com>
2025-07-28 17:52:32 -1000

    Refactor: Improve code formatting and consistency in script.js

--- script.js
+++ script.js
@@ -9,49 +9,46 @@ const pop_input = document.getElementById("pop-input");
 const pop_button = document.getElementById("pop-button");
 const rowContainer = document.getElementById("t_refer");
 
-function empty_row(){
-    if(row_count == 0) return true;
-    else return false;
+function empty_row() {
+  if (row_count == 0) return true;
+  else return false;
 }
 
-function full_row(){
-    if(task_box_count == 3) return true;
-    else return false
+function full_row() {
+  if (task_box_count == 3) return true;
+  else return false;
 }
 
-function create_row(){
-    row_count++;
-    task_box_count = 0;
+function create_row() {
+  row_count++;
+  task_box_count = 0;
 
-    const newRow = document.createElement('div');
-    newRow.className = `row-${row_count}`;
-    newRow.classList.add("ref_row");
-    newRow.id = `row-${row_count}`;
+  const newRow = document.createElement("div");
+  newRow.className = `row-${row_count}`;
+  newRow.classList.add("ref_row");
+  newRow.id = `row-${row_count}`;
 
-    rowContainer.appendChild(newRow);
+  rowContainer.appendChild(newRow);
 }
 
-function deleteTask(deleteButton){
+function deleteTask(deleteButton) {
   const to_delete = deleteButton.parentElement;
-  to_delete.remove();  
+  to_delete.remove();
 }
 
 function addList() {
-
   popup.classList.add("shown");
-  pop_input.focus()
+  pop_input.focus();
 
   pop_button.onclick = function () {
-
-    const pop_input_content = pop_input.value.trim()
+    const pop_input_content = pop_input.value.trim();
 
     if (pop_input_content == "") {
       popup.classList.remove("shown");
     } else {
-
       // verify if there's a row, if not then create one
 
-      if (empty_row() || full_row()){
+      if (empty_row() || full_row()) {
         create_row();
       }
 
@@ -62,7 +59,7 @@ function addList() {
 
       const newTaskBox = document.createElement("div");
       newTaskBox.className = `task-box-${task_box_cnt}`;
-      newTaskBox.classList.add("ref_taskbox")
+      newTaskBox.classList.add("ref_taskbox");
       newTaskBox.innerHTML = `
             <p class="task-box-name">${pop_input_content}</p>
             <div class="task-pos" id="task-pos-${task_box_cnt}"></div>
@@ -74,7 +71,7 @@ function addList() {
       popup.classList.remove("shown");
       pop_input.value = "";
     }
-  }
+  };
 }
 
 // Now The Task Creating Process
@@ -83,18 +80,17 @@ const taskPopup = document.getElementById("pop-up-task-name");
 const taskInput = document.getElementById("pop-task-input");
 const taskButton = document.getElementById("pop-task-button");
 
-function addTask(taskBoxId){
+function addTask(taskBoxId) {
   current_task_box_id = taskBoxId;
   taskPopup.classList.add("shown");
-  taskInput.focus()
+  taskInput.focus();
 
-  taskButton.onclick = function(){
+  taskButton.onclick = function () {
     const taskContent = taskInput.value.trim();
 
-    if(taskContent == ""){
+    if (taskContent == "") {
       taskPopup.classList.remove("shown");
-    }
-    else {
+    } else {
       // create task
 
       const newTask = document.createElement("div");
@@ -105,11 +101,13 @@ function addTask(taskBoxId){
         <box-icon name='x' class="delete-task" onclick="deleteTask(this)"></box-icon>
       `;
 
-      const targetTaskBox = document.getElementById(`task-pos-${current_task_box_id}`);
+      const targetTaskBox = document.getElementById(
+        `task-pos-${current_task_box_id}`
+      );
       targetTaskBox.appendChild(newTask);
 
       taskPopup.classList.remove("shown");
       taskInput.value = "";
     }
-  }
+  };
 }
